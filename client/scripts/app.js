2e21e23aa49ee5c841fc873af6288f2af0d4c13f
dlfjsld1 <ltk900216@gmail.com>
2020-11-12 12:07:50 +0900

    almost end

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,47 +5,100 @@ const app = {
   fetch : () => {
     return fetch(app.server)
     .then(res => res.json())
-    .then(res =>{
-      //console.log();
-      //username, roomname, data, text
-      //result : {[{username: "관리자", roomname: "코드스테이츠", date: "2020-11-11T06:48:12.045Z", text: "이머시브 여러분들 환영합니다"}]}
+    .then(res => {
       for(let el of res.results) {
-        let chat = document.querySelector('#chats')
-        //make box
-        let chatbox = document.createElement('div')
-        chatbox.classList.add('tweet')
-        //insert chatbox in chat
-        chat.prepend(chatbox)
-        
-        //make username
-        let nickname = document.createElement('div')
-        nickname.textContent = el.username
-        nickname.classList.add('username')
-        chatbox.append(nickname)
-
-        //make content
-        let content = document.createElement('div')
-        content.classList.add('text')
-        content.textContent = el.text
-        chatbox.append(content)
-        
-        //make date(time)
-        let date = document.createElement('div')
-        date.classList.add('date')
-        date.textContent = el.date
-        chatbox.append(date)
-
-        //make room
-        let room = document.createElement('div')
-        room.classList.add('roomname')
-        room.textContent = el.roomname
-        chatbox.append(room)
+        app.renderMessage(el);
       }
     })
   },
-  //test
-  send : ()=> {},
-  clearMessages : ()=> {},
-  renderMessage : ()=> {},
+  send : (message)=> {
+    const serverURL = 'http://52.78.206.149:3000/messages'
+    window.fetch(serverURL, {
+      method: 'POST',
+      body: JSON.stringify(message),
+      headers: {
+        "Content-Type": "application/json",
+      }
+    }).then(response => {
+      return response.json();
+    }).then(json => {
+      console.log(json);
+      // message sent!
+    });
+  },
+  clearMessages : ()=> {
+    //just delete text and html with innerHTML
+    const chatbox = document.querySelector('#chats')
+    chatbox.innerHTML = '';
+    //also you can use this
+    //while(chatbox.firstChild) {
+    //chatbox.removeChild(chatbox.firstChild);
+    // }
+  },
+  renderMessage : (data)=> {
+    let chatbox = document.querySelector('#chats')
+    let tweetbox = document.createElement('div')
+    tweetbox.classList.add('tweet')
+    chatbox.prepend(tweetbox)
+    
+    let tweet = document.querySelector('.tweet')
+    
+    let username = document.createElement('div')
+    username.classList.add('username')
+    username.textContent = data.username
+    tweet.append(username)
+
+    let text = document.createElement('div')
+    text.classList.add('text')
+    text.textContent = data.text
+    tweet.append(text)
+
+    let roomname = document.createElement('div')
+    roomname.classList.add('roomname')
+    roomname.textContent = data.roomname
+    tweet.append(roomname)
+
+    let date = document.createElement('div')
+    date.classList.add('date')
+    date.textContent = data.date
+    tweet.append(date)
+  },
+  // select : (para) => {
+  //   let selector = document.querySelector('.room').value
+  //   if(selector === 1) {
+
+  //   }
+  // }
 };
 app.init()
+
+//making send button
+let button = document.querySelector('.sendbutton');
+button.addEventListener('click', () => {
+  let input = document.querySelector('#textarea')
+  const message = {
+    username: 'Im name',
+    text: input.value,
+    date: new Date(),
+    roomname: '111' 
+  };
+  app.send(message)
+  app.clearMessages()
+  app.fetch()
+  input.value = ''
+})
+let selector = document.querySelector('.room');
+selector.addEventListener('change', () => {
+  if(selector.value === 1) {
+    fetch(app.server)
+    .then(res => res.json())
+    .then(res => {
+      app.clearMessages()
+      for(let el of res.results) {
+        if(el.roomname === 1) {
+          app.renderMessage(el);
+        }
+      }
+    })
+  }
+})
